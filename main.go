b3c8a254bda8e25fb3b6c9e5c3dae87611688c30
조상혁 <84455@josanghyeog-ui-MacBookPro.local>
2024-11-06 19:44:53 +0900

    web hook test log

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"invest/app"
 
 	"invest/bot"
@@ -26,6 +27,7 @@ const (
 func main() {
 	// Create a new instance of the server
 
+	fmt.Println("Webhook Test 1")
 	conf, err := config.NewConfig()
 	if err != nil {
 		panic(err)

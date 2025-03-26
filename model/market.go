d2780c12f28434dd5ddc28555cd8f059dfa42f40
chosh901 <chosh901@naver.com>
2025-03-26 17:18:17 +0900

    [modify] Marketlevel range change

--- model/market.go
+++ model/market.go
@@ -15,15 +15,15 @@ var marketLevelList = []string{"MAJOR_BEAR", "BEAR", "VOLATILIY", "BULL", "MAJOR
 func (c MarketLevel) MaxVolatileAssetRate() float64 {
 	switch c {
 	case MAJOR_BEAR:
-		return 0.2
+		return 0.15
 	case BEAR:
-		return 0.3
+		return 0.2
 	case VOLATILIY:
-		return 0.4
+		return 0.25
 	case BULL:
-		return 0.5
+		return 0.3
 	case MAJOR_BULL:
-		return 0.6
+		return 0.4
 	}
 	return 0
 }
@@ -33,13 +33,13 @@ func (c MarketLevel) MinVolatileAssetRate() float64 {
 	case MAJOR_BEAR:
 		return 0.1
 	case BEAR:
-		return 0.2
+		return 0.15
 	case VOLATILIY:
-		return 0.3
+		return 0.2
 	case BULL:
-		return 0.4
+		return 0.25
 	case MAJOR_BULL:
-		return 0.5
+		return 0.3
 	}
 	return 0
 }

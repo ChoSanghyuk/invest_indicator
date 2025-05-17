665ed988f35e65a4eac721318a41268bcaf6faa1
chosh901 <chosh901@naver.com>
2025-05-17 16:34:18 +0900

    .

--- app/middleware/middleware.go
+++ app/middleware/middleware.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"strings"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/rs/zerolog/log"
@@ -9,10 +11,18 @@ import (
 func SetupMiddleware(router fiber.Router) {
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://lomoninvest.shop:50001", // Replace with your Flutter web origin
-		AllowHeaders:     "Origin, Content-Type, Authorization",
-		AllowMethods:     "*",
+		AllowOrigins: "http://www.lomoninvest.shop:50001", // Replace with your Flutter web origin
+		AllowHeaders: "Origin, Content-Type, Authorization",
+		AllowMethods: strings.Join([]string{
+			fiber.MethodGet,
+			fiber.MethodPost,
+			fiber.MethodHead,
+			fiber.MethodPut,
+			fiber.MethodDelete,
+			fiber.MethodPatch,
+		}, ","),
 		AllowCredentials: true,
+		MaxAge:           60 * 60 * 1,
 	}))
 	router.Use(errorHandle)
 	router.Use(logRequest)
